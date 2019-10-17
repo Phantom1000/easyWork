88f15d05bfbec9207735a69587119998ac365d4a
Phantom <nikitka.dankin@mail.ru>
2019-10-18 02:20:41 +0300

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Role::create(
+            ['title' => 'Работодатель'],
+        );
+
+        Role::create(
+            ['title' => 'Фрилансер'],
+        );
     }
 }
